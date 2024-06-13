eec44dcb687aaa13e3ea462991c1e98ee23b3cd7
Ariana <152944814+aam214@users.noreply.github.com>
2024-06-13 12:42:48 -0500

    addCard array function created. Added styling to element of textarea. Increased font-weight in section box.

--- main.js
+++ main.js
@@ -5,6 +5,19 @@ const answer =document.getElementById("answer");
 
 let cardsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
 
+function addCard(){
+let cardInformation = {
+  'my-question' : question.value,
+  'my-answer' : answer.value,
+}
+cardsArray.push(cardInformation);
+localStorage.setItem('items', JSON.stringify(cardsArray));
+cardMaker(cardsArray[cardsArray.length - 1]);
+question.value = '';
+answer.value = '';
+}
+
+
 function resetCards(){
 localStorage.clear();
 studycards.innerHTML = '';
