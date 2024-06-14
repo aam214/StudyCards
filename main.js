bef78b44444193bd7e599b40ca8c9eb39be464cd
Ariana <152944814+aam214@users.noreply.github.com>
2024-06-14 11:54:49 -0500

    Removed main element and added a div in it's place. Added CSS grid and edited styling of study cards in CSS. Edited transition to button hover. Changed width of the create box. Changed var to const in JS. Changed answer to be an h2 element.

--- main.js
+++ main.js
@@ -3,34 +3,31 @@ const createBox = document.getElementsByClassName("create-box")[0];
 const question = document.getElementById("question");
 const answer = document.getElementById("answer");
 
-let cardsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [ ];
+var cardsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
 
 cardsArray.forEach(studyCardMaker);
 
 function studyCardMaker(text){
-  var div = document.createElement('div');
-  var h3_question = document.createElement('h3');
-  var h3_answer = document.createElement('h3');
+  const div = document.createElement('div');
+  const h3_question = document.createElement('h3');
+  const h2_answer = document.createElement('h2');
   div.className = 'studycards';
 
-  h3_question.setAttribute('style', "border-top:1px solid pink");
-  h3_question.innerHTML = text.my_question;
 
-  h3_question.setAttribute("style", "border-bottom: 3px solid rgb(197, 54, 54); padding: 25px 34px; margin-top:20px");
+  h3_question.setAttribute("style", "padding: 25px 38px");
   h3_question.textContent = text.my_question;
 
-  h3_answer.setAttribute("style", "text-align:center; display:none; color:red");
-  h3_answer.textContent = text.my_answer;
+  h2_answer.setAttribute("style", "text-align:center; display:none; color: red");
+  h2_answer.textContent = text.my_answer;
 
 div.appendChild(h3_question);
-div.appendChild(h3_answer);
+div.appendChild(h2_answer);
 
-
-div.addEventListener("click", function(){
-  if (h3_answer.style.display == 'none')
-    h3_answer.style.display = "block";
+div.addEventListener("click", ()=> {
+  if (h2_answer.style.display == "none")
+    h2_answer.style.display = "block";
   else
-  h3_answer.style.display= "none";
+  h2_answer.style.display= "none";
 });
 studycards.appendChild(div);
 }
