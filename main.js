c557c62b1b78773e3ec373b20ed47d7711e99c0b
Ariana <152944814+aam214@users.noreply.github.com>
2024-06-14 10:50:36 -0500

    Added emoji to Reset button. Organized JS. Edited set attribute styles to study card. Added an h3 and div in JS. Edited name of my-question and my-answer. Place an event listener when study card clicked to show answer and hide answer. Edited CSS for class of studycards.

--- main.js
+++ main.js
@@ -1,21 +1,44 @@
 const studycards = document.getElementsByClassName("studycards")[0];
 const createBox = document.getElementsByClassName("create-box")[0];
-const question =document.getElementById("question");
-const answer =document.getElementById("answer");
+const question = document.getElementById("question");
+const answer = document.getElementById("answer");
 
-let cardsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
+let cardsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [ ];
 
 cardsArray.forEach(studyCardMaker);
+
 function studyCardMaker(text){
-  let div = document.createElement("div");
-  let h3_question = document.createElement('h3');
-  let h3_answer = document.createElement('h3');
+  var div = document.createElement('div');
+  var h3_question = document.createElement('h3');
+  var h3_answer = document.createElement('h3');
+  div.className = 'studycards';
+
+  h3_question.setAttribute('style', "border-top:1px solid pink");
+  h3_question.innerHTML = text.my_question;
+
+  h3_question.setAttribute("style", "border-bottom: 3px solid rgb(197, 54, 54); padding: 25px 34px; margin-top:20px");
+  h3_question.textContent = text.my_question;
+
+  h3_answer.setAttribute("style", "text-align:center; display:none; color:red");
+  h3_answer.textContent = text.my_answer;
+
+div.appendChild(h3_question);
+div.appendChild(h3_answer);
+
+
+div.addEventListener("click", function(){
+  if (h3_answer.style.display == 'none')
+    h3_answer.style.display = "block";
+  else
+  h3_answer.style.display= "none";
+});
+studycards.appendChild(div);
 }
 
 function addCard(){
 let cardInformation = {
-  'my-question' : question.value,
-  'my-answer' : answer.value,
+  'my_question' : question.value,
+  'my_answer' : answer.value,
 }
 cardsArray.push(cardInformation);
 localStorage.setItem('items', JSON.stringify(cardsArray));
