9359b1974df04eaa9be9a07b9dfb9e2030838bbd
Ariana <152944814+aam214@users.noreply.github.com>
2024-06-19 09:35:30 -0500

    Update main.js

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const createBox = document.getElementsByClassName("create-box")[0];
 
 const question = document.getElementById("question");
 const answer = document.getElementById("answer");
+
 let cardsArray = localStorage.getItem('items')?
 JSON.parse(localStorage.getItem('items')) : [];
 
